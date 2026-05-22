c9cc3b80789f3645c71a2019c293ddaa4ab88a67
LETELA-DAN <your-email@example.com>
2026-05-22 15:11:14 +0300

    addition of signup.php

---
+++ signup.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_GET['message'])) {
+    $message=$_GET['message'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project zeron</title>
+    <link rel="stylesheet" href="./CSS/styles.css">
+</head>
+<body>
+   <header>
+            <h1>Project Zeron</h1>
+        </header>
+
+   <main>
+    <form action="login.php" method="post">
+       <div class="logo">
+                <img src="./user_profiles/test_upload.jpg" alt="Logo" width="80">
+            </div>
+            <h2 id="details">login as: user</h2>
+            <p class="error"><?php if (isset($message)) { echo $message; } ?></p>
+        <input type="email" id="email" name="email" placeholder="Enter your email"><br>
+        <input type="password" name="password" id="password" placeholder="Enter your password">
+        <input type="hidden" name="role" value="user" id="role-input">
+        <input type="submit" value="Submit">
+        <p>Don't have an account? <a href="signup.html">Sign up</a></p>
+    </form>
+   </main>
+    
+</body>
+</html>
